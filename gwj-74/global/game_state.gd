f92d3ff98ea7f244f7836772305c75278badbcb1
s4zz <philippbecker1@web.de>
2024-10-19 01:09:02 +0200

    added

--- gwj-74/global/game_state.gd
+++ gwj-74/global/game_state.gd
@@ -8,3 +8,7 @@ var intro_scary_color: bool = false:
 	set(value):
 		intro_scary_color = value
 		intro_scary_color_changed.emit(value)
+		
+func set_color(timer: float, to_scary: bool) -> void:
+	await get_tree().create_timer(timer).timeout
+	intro_scary_color = to_scary
