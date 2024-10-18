652d741183bfee6e68c4fa4487e54f3e76bc4f83
OliPohl <oliver@pohl66.de>
2024-10-18 21:33:46 +0200

    intro color fade

--- gwj-74/global/game_state.gd
+++ gwj-74/global/game_state.gd
@@ -2,3 +2,9 @@ extends Node
 
 var test_checked: bool = false
 var was_outside: bool = false
+
+signal intro_scary_color_changed
+var intro_scary_color: bool = false:
+	set(value):
+		intro_scary_color = value
+		intro_scary_color_changed.emit(value)
