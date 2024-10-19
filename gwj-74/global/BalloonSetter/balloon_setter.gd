767f07ca976e32d2d4f79787352ecebfb1f9120a
OliPohl <oliver@pohl66.de>
2024-10-19 14:12:53 +0200

    balloon setter

---
+++ gwj-74/global/BalloonSetter/balloon_setter.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@export var normal_balloon_path : String = "res://scenes/dialogue/balloon.tscn"
+@export var color_screen_balloon_path : String = "res://scenes/dialogue/color_screen_balloon.tscn"
+
+
+func _ready() -> void:
+	set_balloon(false)
+
+
+func set_balloon(is_color_screen : bool) -> void:
+	if is_color_screen:
+		DialogueManager.DialogueSettings.set_setting(&"balloon_path", color_screen_balloon_path)
+	else:
+		DialogueManager.DialogueSettings.set_setting(&"balloon_path", normal_balloon_path)
