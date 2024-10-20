26d5c0e195c8fada81947550d6d1af7f6d5c8a59
s4zz <philippbecker1@web.de>
2024-10-20 19:06:35 +0200

    added

--- gwj-74/scenes/environment/inside_door_closer.gd
+++ gwj-74/scenes/environment/inside_door_closer.gd
@@ -2,6 +2,9 @@ extends Area2D
 
 @export var train : Train = null
 
+@export var dialogue_resource: DialogueResource
+@export var dialogue_start: String = "train_door_close"
+
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -12,3 +15,4 @@ func _on_body_entered(body: Node) -> void:
 		return
 	if train.door_inside_open and GameState.train_key_broken:
 		train.call_deferred("set_door_inside", false)
+		DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start)
