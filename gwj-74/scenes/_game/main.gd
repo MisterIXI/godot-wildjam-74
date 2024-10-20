a29926a47625eeddd766d777311221143307a952
s4zz <philippbecker1@web.de>
2024-10-20 21:08:12 +0200

    added sfx

--- gwj-74/scenes/_game/main.gd
+++ gwj-74/scenes/_game/main.gd
@@ -1,6 +1,13 @@
 extends Node2D
 class_name Main
 @onready var deserted_subway_sfx: AudioStreamPlayer = %DesertedSubway
+@onready var creepy_ghost: AudioStreamPlayer = $SFX/CreepyGhost
+@onready var creepy_sound: AudioStreamPlayer = $SFX/CreepySound
+@onready var locked_door: AudioStreamPlayer = $SFX/LockedDoor
+@onready var unlock_door: AudioStreamPlayer = $SFX/UnlockDoor
+@onready var key_rattle: AudioStreamPlayer = $SFX/KeyRattle
+
+
 @onready var environment: xXxEnvironmentxXx = %Environment
 @onready var player: Player = %player
 @onready var padlock: Padlock = %Padlock
