dff4574784e1acbbc41847a579cabf4adb0477cc
Yannik Braendle <yannik.braendle@t-online.de>
2024-10-20 20:11:09 +0200

    item ui visible fix

--- gwj-74/scenes/ui/item_ui.gd
+++ gwj-74/scenes/ui/item_ui.gd
@@ -10,14 +10,14 @@ func _ready():
 ## Called when the GameState signals that the UI flags have changed and toggles the visibility of the items.
 func _on_ui_flags_changed():
 	if GameState.has_train_key and not GameState.train_key_broken:
-		key_train.visible = true
+		key_train.set_deferred("visible", true)
 	else:
-		key_train.visible = false
+		key_train.set_deferred("visible", false)
 	if GameState.has_toilette_key and not GameState.toilette_key_used:
-		key_toilette.visible = true
+		key_toilette.set_deferred("visible", true)
 	else:
-		key_toilette.visible = false
+		key_toilette.set_deferred("visible", false)
 	if GameState.has_ticked:
-		ticket.visible = true
+		ticket.set_deferred("visible", true)
 	else:
-		ticket.visible = false
+		ticket.set_deferred("visible", false)
